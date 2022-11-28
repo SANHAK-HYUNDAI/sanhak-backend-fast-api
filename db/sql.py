2cbac3bed27262451247fe3469fbe896ccaeee8b
tianea2160 <rhlehfndvkd7557@daum.net>
2022-11-28 10:42:16 +0900

    fix : fix sql column name and add ca bulk insert

--- db/sql.py
+++ db/sql.py
@@ -1,16 +1,26 @@
 bulk_insert_ro_sql = """
 insert into repair_order (
-vehicle_type,
-cause,
-part_number,
-cause_part,
-cause_part_name_kor,
-cause_part_name_eng,
-cause_part_cluster,
-symptom,
-pecial_note,
-location,
-problematic_situation,
-created_at,
-modified_at 
+    big_phenom          ,
+    cause               ,
+    cause_part          ,
+    cause_part_cluster  ,
+    cause_part_name_eng ,
+    cause_part_name_kor ,
+    location            ,
+    part_number         ,
+    problematic         ,
+    special_note        ,
+    sub_phenom          ,
+    vehicle_type        
+) values """
+
+bulk_insert_ca_sql = """
+insert into cafe_article(
+board_name              ,
+cafe_name               ,
+content                 ,
+created_at              ,
+title                   ,
+url                     ,
+writer 
 ) values """
